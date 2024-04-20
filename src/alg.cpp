d5d34f690618a9e16964f32d5b7caf688f9110b9
VeraVerendyaykina <151723475+VeraVerendyaykina@users.noreply.github.com>
2024-04-20 18:52:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,47 @@
-// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+#include <string>
+
+template<typename T, int size>
+class TPQueue {
+ private:
+    int nachalo, konec, counter;
+    T *data;
+
+ public:
+    TPQueue() : nachalo(0), konec(0), counter(0), data(new T[size]) {}
+
+    T pop() {
+        if (counter == 0) {
+            throw std::string("Empty!!!");
+        } else {
+            counter--;
+            return data[nachalo++ % size];
+        }
+    }
+
+    void push(const T &item) {
+        if (counter >= size)
+            throw std::string("Full!!!");
+        counter++;
+        int Index = konec;
+        for (int i = nachalo; i < konec; i++) {
+            if (data[i].prior < item.prior) {
+                Index = y;
+                break;
+            }
+        }
+        for (int y = konec; i > Index; y--)
+            data[y % size] = data[(y - 1) % size];
+        data[Index % size] = item;
+        konec++;
+    }
+};
+
+struct SYM {
+    char ch;
+    int prior;
+};
+
+#endif  // INCLUDE_TPQUEUE_H_// Copyright 2021 NNTU-CS
